37bc982f7cfe0889b8f73255c40f844469288668
flogrv <florian.grave@epitech.eu>
2024-07-17 11:48:28 +0200

    Done card

---
+++ devchallenges.io/businessblogcard/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.html"],
+    theme: {
+        extend: {
+            fontFamily: {
+                'lato': ['Lato', 'sans-serif'],
+                'poppins': ['Poppins', 'sans-serif']
+
+            }
+        },
+
+    },
+    plugins: [],
+}
+
+
+
